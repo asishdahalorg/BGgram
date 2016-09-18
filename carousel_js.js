4d8faee33d1a7bc8d0a9c1b207536dd05e0814a3
Kimberly Perez <kimberly_93pc@Kimberly.local>
2016-09-18 18:54:44 -0400

    changed home with jquery

---
+++ carousel_js.js
@@ -0,0 +1,96 @@
+
+var jsonData = [{"src":"images/home1.jpg","alt":"photo1"},
+				{"src":"images/home2.jpg","alt":"photo2"},
+				{"src":"images/home3.jpg","alt":"photo3"},
+				{"src":"images/home4.jpg","alt":"photo4"},
+				{"src":"images/home5.jpg","alt":"photo5"},
+				{"src":"images/home6.jpg","alt":"photo6"},
+				{"src":"images/home7.jpg","alt":"photo7"},
+				{"src":"images/home8.jpg","alt":"photo8"},
+				{"src":"images/home9.jpg","alt":"photo9"}
+				];
+
+$(function(){
+	function bigPhotos(){
+	var photos = '<div class="item"><img src="#src" alt="#alt"></div>';
+	var thumbPhotos = '<div class="img-t"  data-photo="#data-photo#"><img src="#src" alt="#alt"/></div>';
+	for(var i = 0; i<jsonData.length; i++)
+	{
+		var newSlide = photos.replace("#src",jsonData[i].src).replace("#alt",jsonData[i].alt);
+		$(".slideShow").append(newSlide);
+		var newThumbnails = thumbPhotos.replace("#src",jsonData[i].src).replace("#alt",jsonData[i].alt).replace("#data-photo#",i);
+		$(".thumbnails .thumbnailsSingle").append(newThumbnails);
+	}
+	$(".slideShow div:first").addClass("active");
+	$(".thumbnailsSingle div:first").addClass("active");
+
+	$(".thumbnailsSingle div.img-t").click(
+	function(){
+		clearInterval(slideShowTimeOut);
+
+		var index = $(this).attr("data-photo");
+		$(".slideShow .active").removeClass("active");
+		$(".thumbnailsSingle .active").removeClass("active");
+		$(".slideShow div").eq(index).addClass("active");
+		$(".thumbnailsSingle div").eq(index).addClass("active");
+		slideShowTimeOut = setInterval(function(){leftFunct();},5000);
+	});
+
+}
+bigPhotos();
+
+function rightFunct(){
+	var active = $(".slideShow .active");
+	var activeThumb = $(".thumbnailsSingle .active");
+	var nexLength = $(".slideShow .active").next().length;
+	if(nexLength==0)
+	{
+		active.removeClass("active");
+		$(".slideShow div:first").addClass("active");
+		activeThumb.removeClass("active");
+		$(".thumbnailsSingle div:first").addClass("active");
+	}
+	else 
+	{
+		active.removeClass("active").next().addClass("active");
+		activeThumb.removeClass("active").next().addClass("active");
+	}
+}
+function leftFunct(){
+	var active = $(".slideShow .active");
+	var activeThumb = $(".thumbnailsSingle .active");
+	var prevLength = $(".slideShow .active").prev().length;
+	if(prevLength==0)
+	{
+		active.removeClass("active");
+		$(".slideShow div:last").addClass("active");
+		activeThumb.removeClass("active");
+		$(".thumbnailsSingle div:last").addClass("active");
+	}
+	else 
+	{
+		active.removeClass("active").prev().addClass("active");
+		activeThumb.removeClass("active").prev().addClass("active");
+	}
+}
+$(".right").click(
+	function()
+	{
+		clearInterval(slideShowTimeOut);
+		rightFunct();
+		slideShowTimeOut = setInterval(function(){leftFunct();},5000);
+	});
+$(".left").click(
+	function()
+	{
+		clearInterval(slideShowTimeOut);
+		leftFunct();
+		slideShowTimeOut = setInterval(function(){leftFunct();},5000);
+	});
+
+	var slideShowTimeOut = setInterval(function(){rightFunct();},5000);
+
+
+
+
+});
