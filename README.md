72b111f9a4f2b99e512b37b0d303a534e8314924
Asish Dahal <Asish Dahal>
2016-09-20 04:18:06 -0400

    Previous catalog.html and catalog.css moved into /previous. New catalog features dynamic page

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# BGgram
+###BGgram
+
+Welcome to BGgram, home of the best and most user friendly online image
+and background editor. Choose your like from millions of trending images, 
+and edit it to your desires.
+
+The main entry point is index.html
+All other html files are in /html
+All css files are in /css
+All used script files are in /script 
+All local images are in /images
+
+Currently used API:
+* Flickr
+
+You may access the site from:
+* `https://bluedrag.github.io/BGgram/`
+
+The github repository is located at
+* https://github.com/bluedrag/BGgram
+
+The github releases are located at
+* https://github.com/bluedrag/BGgram/releases
+
--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,28 +1,63 @@
-$('#input input').unbind().keyup(function(e) {
-    var value = $(this).val();
-    if (value.length>3) {
-        table.search(value).draw();
-    } else {
-        //optional, reset the search if the phrase
-        //is less then 3 characters long
-        table.search('').draw();
-    }
-});
+"use strict";
+$(document).ready();
+//The top 2 lines make sure that syntax is strictly controlled
+//and that all elements are ready to be used before the script begins
 
-$(document).ready(function () {
-    $("#searchbutton").click(function () {
-        $("#images").empty();
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-            {
-                tags: "car",
-                tagmode: "any",
-                format: "json"
-            }, function (data) {
-                $.each(data.items, function (i, item) {
-                    $('<img/>').attr("src", item.media.m).appendTo('#images');
-                    if (i == 3) return false;
-                });
+// A mock database of images for catalog. This is NOT used for HW2. We ended up using the Flickr public api instead
+var images =   [{"src":"../images/catalog1.jpg","alt":"photo1","tag":"food"},
+                {"src":"../images/catalog2.jpg","alt":"photo2","tag":"food"},
+                {"src":"../images/catalog3.jpg","alt":"photo3","tag":"food"},
+                {"src":"../images/catalog4.jpg","alt":"photo4","tag":"food"},
+                {"src":"../images/catalog5.jpg","alt":"photo5","tag":"food"},
+                {"src":"../images/catalog6.jpg","alt":"photo6","tag":"food"},
+                {"src":"../images/catalog7.jpg","alt":"photo7","tag":"food"},
+                {"src":"../images/catalog8.jpg","alt":"photo8","tag":"food"},
+                {"src":"../images/catalog9.jpg","alt":"photo9","tag":"food"},
+                {"src":"../images/catalog2.jpg","alt":"photo10","tag":"food"},
+                {"src":"../images/catalog10.jpg","alt":"photo11","tag":"food"},
+                {"src":"../images/catalog11.jpg","alt":"photo12","tag":"food"},
+                {"src":"../images/catalog12.jpg","alt":"photo13","tag":"food"},
+                {"src":"../images/catalog13.jpg","alt":"photo14","tag":"food"},
+                {"src":"../images/catalog14.jpg","alt":"photo15","tag":"food"},
+                {"src":"../images/catalog15.jpg","alt":"photo16","tag":"food"},
+                {"src":"../images/catalog16.jpg","alt":"photo17","tag":"food"}];
+
+//A function to insert one single tile of image
+function insert_img_thumbnail_into_gallery(i,link,title) {
+    $(document).ready();
+    //this makes it easier to see how the structure is being created
+    var $column = $('<div></div>').attr("class","col-lg-3 col-md-4 col-sm-6 col-xs-12 thumb");
+    var $link_to_image = $("<a></a>").attr("class","thumbnail imglink" +i).attr("href",link);
+    var $thumbnail_of_image = $("<img/>").attr("class","img-thumbnail img-responsive img-rounded page_image").attr("src",link);
+    var $image_title = $("<div></div>").attr("class","ts").text(title);
+
+    //the main coloumn is appended into the row
+    $link_to_image.append($thumbnail_of_image,$image_title);
+    $column.append($link_to_image).appendTo("#galleryrow");
+
+}
+
+function flickr() {
+    //redundant? check to make sure elements are all set
+    $(document).ready();
+    $.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        {   //I tried to implement a search functionality by
+            //accessing $("#search_bar).val() which is the input value from catalog page.
+            //however, the tag does not seem to like this, or rather, it does not evaluate the string
+            //before being parsed.
+            //The good news is, leaving tags blank acts as sort of a dice roll so clicking search will
+            //still cause dynamic change
+            tags: "",
+            tagmode: "any",
+            format: "json"
+        }, function (data){
+            //a jquery forloop over data returned by flickr
+            $.each(data.items,function (i,item){
+                //for each image, insert a set of elements
+                insert_img_thumbnail_into_gallery(i,item.media.m,item.title)
             });
 
-    });
-});
+        });
+}
+
+flickr();
