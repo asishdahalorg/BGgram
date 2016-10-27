e5de9bf3e907b26b117493c36d39decce21b1ab0
Kimberly Perez <kimberly_93pc@Kimberly.local>
2016-10-27 09:47:09 -0400

    Added Cloud and changed nod.js

--- nodeApp.js
+++ nodeApp.js
@@ -1,8 +1,10 @@
-var user;
-var initialized = false;
-var firebase;
-var finmenu;
-var firebase = require("firebase");
+'use strict'
+
+var firebase = require("firebase/");
+var auth = require("firebase/auth");
+var database = require("firebase/database");
+var storage = require("firebase/storage");
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
@@ -20,8 +22,10 @@ app.get("'", function(req,res){
 	res.send("Hello");
 });
 var port = process.env.PORT || 3000;
+
 app.use('/static', express.static(__dirname + '/public'));
-path = require('path');
+
+var path = require('path');
 app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname,'html/','index.html'))
 });
@@ -29,3 +33,39 @@ app.get('/',function(req,res){
 app.listen(port, function(){
 console.log("Example");
 });
+
+var gcloud = require('@google-cloud/storage')({
+	projectId: "bggram-d9ba0"
+});
+
+
+
+app.put('/uploads', function (req, res) {
+    console.log("Client wants to update.");
+
+    var photo = req.body.fileInput;
+	var gcs = gcloud.storage();
+	var bucket = gcs.bucket('gs://bggram-d9ba0.appspot.com/'+firebase.User.uid);
+	bucket.upload(photo, function(err, file) {
+  
+	});
+
+
+    // var storageRef = storage.ref("Photo/" + firebase.User.uid);
+    // var imagesRef = storageRef.child(photo.name);
+    // // updateDatabase(photo.name, privacy, theme);
+    // imagesRef.put(photo).then(function(snapshot) {
+    //       console.log('Uploaded Photo!');
+    //       window.location.reload();
+    //     });
+    console.log('Uploaded Photo!');
+
+    res.send("ok!")
+
+});
+// app.get('/static/scripts/uploads.js',function(req,res){
+// 	console.log("here");
+// 	global.snapshot = etriveData.on(function (snapshot) {return snapshot;});
+// 	res.send(snapshot);
+// });
+
--- public/scripts/nodeApp.js
+++ public/scripts/nodeApp.js
@@ -2,42 +2,47 @@ var user;
 var initialized = false;
 var firebase;
 var finmenu;
-var firebase = require("firebase");
+const FirebaseREST = require("firebase-rest").default;
 var express = require('express');
+var uploads = require('uploads.js');
 var app = express();
 var bodyParser = require('body-parser');
-var snapshot;
+global.snapshot = require("snapshot")
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-
-firebase.initializeApp({
-    serviceAccount: "privkey.json",
-    databaseURL: "https://bggram-d9ba0.firebaseio.com/"
-});
+console.log("here");
+FirebaseREST.client("https://bggram-d9ba0.firebaseio.com/",{auth:'SECRET'});
 
 
 var port = process.env.PORT || 3000;
 
 path = require('path');
-app.use(express.static(path.join(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.listen(port, function(){
-console.log("On Server");
+
+
+var retriveData = FirebaseREST.database().ref('uers/');
+
+app.get('/static/scripts/uploads.js',function(req,res){
+	sys.puts(sys.inspect(req));
+	req.body.snapshot = etriveData.on(function (snapshot) {return snapshot;});
+	res.send(snapshot);
 });
 
-var retriveData = firebase.database().ref('uers/');
-
-app.get('/uploads', function (req, res) {
-    console.log("Client wants to get");
-    retriveData.on('value', function (snapShot){
-    	snapshot = snapShot;
-    }).catch(function(){
-        res.status(403);
-        res.send();
-    });
-});
+// app.get(function(){
+// 	$get(retriveData,function(req,res){
+// 		global.snapshot = req;
+// 		console.log(res);
+// 	});
+// });
+// FirebaseREST.get('retriveData').then(function(){
+// 	snapshot = body;
+// }).fail(function(){
+// 	console.log(err);
+// });
 
 // app.get('/emptyHtml.html', function (req, res) {
 //     console.log("Requested empty html");
