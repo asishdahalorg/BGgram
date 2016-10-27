e4daca0d3d31539665cb9962572c0c0d2d7361fc
Kimberly Perez <kimberly_93pc@hotmail.com>
2016-10-27 17:43:52 -0400

    Fixed some stuff

--- nodeApp.js
+++ nodeApp.js
@@ -1,9 +1,6 @@
 'use strict'
 
 var firebase = require("firebase/");
-var auth = require("firebase/auth");
-var database = require("firebase/database");
-var storage = require("firebase/storage");
 
 var express = require('express');
 var app = express();
@@ -17,13 +14,12 @@ firebase.initializeApp({
     serviceAccount: "privkey.json",
     databaseURL: "https://bggram-d9ba0.firebaseio.com/"
 });
-// var fireRef = firebase.database().ref('todos');
+
 app.get("'", function(req,res){
 	res.send("Hello");
 });
 var port = process.env.PORT || 3000;
-
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 var path = require('path');
 app.get('/',function(req,res){
@@ -39,33 +35,16 @@ var gcloud = require('@google-cloud/storage')({
 });
 
 
-
 app.put('/uploads', function (req, res) {
     console.log("Client wants to update.");
 
     var photo = req.body.fileInput;
-	var gcs = gcloud.storage();
-	var bucket = gcs.bucket('gs://bggram-d9ba0.appspot.com/'+firebase.User.uid);
+
+	var bucket = gcloud.Storage.bucket('/bggram-d9ba0.appspot.com/Photo/'+firebase.User.uid);
 	bucket.upload(photo, function(err, file) {
   
 	});
-
-
-    // var storageRef = storage.ref("Photo/" + firebase.User.uid);
-    // var imagesRef = storageRef.child(photo.name);
-    // // updateDatabase(photo.name, privacy, theme);
-    // imagesRef.put(photo).then(function(snapshot) {
-    //       console.log('Uploaded Photo!');
-    //       window.location.reload();
-    //     });
     console.log('Uploaded Photo!');
-
     res.send("ok!")
 
 });
-// app.get('/static/scripts/uploads.js',function(req,res){
-// 	console.log("here");
-// 	global.snapshot = etriveData.on(function (snapshot) {return snapshot;});
-// 	res.send(snapshot);
-// });
-
